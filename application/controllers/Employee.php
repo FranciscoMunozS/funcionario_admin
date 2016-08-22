340ac02c55befbc2fc9af04d92408849b85e2ac2
FranciscoMunozS <franciscomuniozs@gmail.com>
2016-08-22 16:08:20 -0300

    2nd commit

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -6,7 +6,7 @@ class Employee extends CI_Controller {
   public function __construct()
   {
     parent::__construct();
-    $this->load->model('Employee_model');
+    $this->load->model('employee_model');
     $this->load->database();
   }
   public function index() {
